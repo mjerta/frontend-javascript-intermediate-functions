463dfa8664e0603c5ca53462091537fb9a3720e3
mjerta <maarten.postma1000@gmail.com>
2024-06-28 13:41:17 +0200

    Clean up and refactor code functions and scope.js

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -3,8 +3,10 @@
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
 
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+const multipleArr = [grades, [6, 4, 5], [8, 9, 4, 6, 10]]
 
-/* Opdracht  1: Cum Laude */
+
+/* Opdracht  1 - Cum Laude */
 
 /* 1a: Script schrijven  */
 // De administratie moet weten hoeveel studenten er dit blok cum laude zijn afgestudeerd (8 of hoger). Daar moeten namelijk speciale diploma's voor besteld worden.
@@ -26,7 +28,8 @@ function amountOfDiplomasCumLaude() {
   return validatedArray.length;
 }
 
-console.log(amountOfDiplomasCumLaude(grades));
+console.log("Opdracht 1a: Cum Laude ")
+console.log(amountOfDiplomasCumLaude());
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
@@ -38,8 +41,8 @@ console.log(amountOfDiplomasCumLaude(grades));
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+
 function cumLaude(grades) {
-  console.log(grades);
   const validatedArray = [];
   for (let i = 0; i < grades.length; i++) {
     if (grades[i] >= 8) {
@@ -48,8 +51,11 @@ function cumLaude(grades) {
   }
   return validatedArray.length;
 }
+console.log("\nOpracht 1b - Omschrijven tot een herbruikbare functie")
 
-console.log(cumLaude(grades));
+for (let i = 0; i < multipleArr.length; i++) {
+  console.log(cumLaude(multipleArr[i]));
+}
 
 /* Opdracht  2: Gemiddeld cijfer */
 
@@ -63,7 +69,7 @@ console.log(cumLaude(grades));
 
 // ---- Verwachte uitkomst: 6.642857142857143
 
-function averageGradeWithoutParemeter() {
+function averageGradeWithoutParameter() {
   let sumOfGrades = 0;
   for (let i = 0; i < grades.length; i++) {
     sumOfGrades += grades[i];
@@ -71,30 +77,30 @@ function averageGradeWithoutParemeter() {
   return sumOfGrades / grades.length;
 }
 
-console.log(averageGradeWithoutParemeter());
+console.log("\nOpracht 2a - Script schrijven")
+console.log(averageGradeWithoutParameter());
 
 /* 2b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd averageGrade, die een array van cijfers verwacht (zoals grades) en het gemiddelde cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 2a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
-const multipleArr = [grades, [6, 4, 5], [8, 9, 4, 6, 10]]
 
-function averageGrade(grades) {
-  let sumOfGrades = 0;
-  for (let i = 0; i < grades.length; i++) {
-    sumOfGrades += grades[i];
-  }
-  return Math.round(sumOfGrades / grades.length);
-}
-
-for (let i = 0; i < multipleArr.length; i++) {
-  console.log(averageGrade(multipleArr[i]));
-}
 
 // ---- Verwachte uitkomsten:
 // averageGrade(grades) geeft 6.642857142857143
 // averageGrade([6, 4, 5]) geeft 5
 // averageGrade([8, 9, 4, 6, 10]) geeft 7.4
+function averageGrade(grades) {
+  let sumOfGrades = 0;
+  for (let i = 0; i < grades.length; i++) {
+    sumOfGrades += grades[i];
+  }
+  return sumOfGrades / grades.length;
+}
+console.log("\nOpracht 2b- Omschrijven tot een herbruikbare functie")
+for (let i = 0; i < multipleArr.length; i++) {
+  console.log(`Array number: ${averageGrade(multipleArr[i])}`);
+}
 
 /* 2c: Afronden op twee decimalen */
 // Zorg ervoor dat het gemiddelde cijfer dat wordt teruggegeven uit de functie netjes wordt afgerond op twee decimalen.
@@ -107,9 +113,9 @@ function averageGradeWIthTwoDecimals(grades) {
   }
   return Math.round((sumOfGrades / grades.length) * 100) / 100;
 }
-
+console.log("\nOpracht 2c- Afronden op twee decimalen")
 for (let i = 0; i < multipleArr.length; i++) {
-  console.log(averageGradeWIthTwoDecimals(multipleArr[i]));
+  console.log(`Array number: ${averageGradeWIthTwoDecimals(multipleArr[i])}`);
 }
 
 /* Bonusopdracht: hoogste cijfer */
@@ -133,6 +139,7 @@ function highestGradeWithoutParameter() {
   }
   return highestNumber;
 }
+console.log("\nOpdracht 3a");
 console.log(highestGradeWithoutParameter());
 
 /* 3b: Omschrijven tot een herbruikbare functie */
@@ -144,7 +151,6 @@ console.log(highestGradeWithoutParameter());
 // highestGrade(grades) geeft 9
 // highestGrade([6, 4, 5]) geeft 6
 // highestGrade([8, 9, 4, 6, 10]) geeft 10
-const mulitpleArrTwo = [grades, [6,4,5], [8, 9, 4, 6, 10]];
 
 function highestGrade(grades) {
   // This will give the first input as the reference. It will replaced if a higher number has been found.
@@ -157,6 +163,7 @@ function highestGrade(grades) {
   return highestNumber;
 }
 
-for (let i = 0; i < mulitpleArrTwo.length; i++) {
-  console.log(`Highest number of array ${i}: ${highestGrade(mulitpleArrTwo[i])}`);
+console.log("\nOpracht 3b - Omschrijven tot een herbruikbare functie")
+for (let i = 0; i < multipleArr.length; i++) {
+  console.log(`Array number: ${highestGrade(multipleArr[i])}`);
 }
